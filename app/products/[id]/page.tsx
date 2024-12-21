98bf29dd59164dbcbcdc276f18849136dd0f89c7
nolan <nolan@mac.com>
2024-12-22 01:23:11 +0900

    fix for deploy

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -9,7 +9,6 @@ export default async function ProductPage({ params }: {
   params: Promise<{ id: string, page: string }>;
 }) {
   const proId = (await params).id;
-  const page = (await params).page;
   const product = await getProductById(parseInt(proId));
 
   if (!product) {
